cfc7424a0a07e3a1669022628b316a76061552c2
sgbaotran <sttr2130@gmail.com>
2023-08-31 01:47:57 -0400

    fd

--- app/(dashboard)/(route)/dashboard/page.tsx
+++ app/(dashboard)/(route)/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { ArrowRight } from 'lucide-react'
 
 const DashboardPage = () => {
   const router = useRouter()
-  
+
 
   return (
     <div className="pb-4">
@@ -25,7 +25,7 @@ const DashboardPage = () => {
 
         {/* Card Item */}
         {tools.map((tool) => (
-          <Card onClick={() => { router.push(tool.href) }} className='second-purple text-slate-200 p-4 border-black/5 flex items-center
+          <Card key={tool.href} onClick={() => { router.push(tool.href) }} className='second-purple text-slate-200 p-4 border-black/5 flex items-center
            justify-between hover:shadow-md transition '>
 
             <div className='flex items-center gap-x-4'>
